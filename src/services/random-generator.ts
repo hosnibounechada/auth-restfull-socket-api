9162dbd83c1dfcdbfc415a32458150395c118377
Hosni Bounechada <hosni.bounechada@gmail.com>
2022-10-18 01:11:47 +0100

    fix username whitespace bug

--- src/services/random-generator.ts
+++ src/services/random-generator.ts
@@ -3,7 +3,8 @@ import { randomInt } from "crypto";
 export class RandomGenerator {
   static username(firstName: string, lastName: string) {
     const uuid = randomInt(1000, 9999);
-    return `${firstName}.${lastName}${uuid}`;
+    const prefix = `${firstName}.${lastName}`;
+    return `${prefix.replace(/\s/g, "")}${uuid}`;
   }
   static randomInt(min: number, max: number) {
     return randomInt(min, max);
