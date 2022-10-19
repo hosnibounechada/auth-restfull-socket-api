643739479372d235fac6e7c6e8dac96c3633e3dd
Hosni Bounechada <hosni.bounechada@gmail.com>
2022-10-20 00:58:13 +0100

    sendMessage & updateMessage validators

--- src/validators/message.ts
+++ src/validators/message.ts
@@ -1,7 +1,10 @@
-import { body, param } from "express-validator";
+import { body, param, query } from "express-validator";
+
+export const idValidator = (input: string) => {
+  return [param(input).isMongoId().withMessage("Invalid inputs")];
+};
 
 export const sendMessageValidator = [
-  body("from").isMongoId().withMessage("Invalid Message"),
   body("to").isMongoId().withMessage("Invalid Message"),
   body("type")
     .matches(/\b(?:text|image)\b/)
@@ -17,6 +20,4 @@ export const updateMessageValidator = [
   body("content").isLength({ min: 1, max: 500 }).withMessage("Invalid Message"),
 ];
 
-export const idValidator = (input: string) => {
-  return [param(input).isMongoId().withMessage("Invalid inputs")];
-};
+export const getMessagesValidator = [param("id").isMongoId().withMessage("Invalid inputs")];
