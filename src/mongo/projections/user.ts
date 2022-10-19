183388840d2e6c30b4a1b0002d4b435fa7b7a11a
Hosni Bounechada <hosni.bounechada@gmail.com>
2022-10-19 02:27:35 +0100

    mongo directory for mongodb pipe-lines and projection functions

---
+++ src/mongo/projections/user.ts
@@ -0,0 +1,28 @@
+import { ProjectionType } from "mongoose";
+import { UserDoc } from "../../models";
+
+export function getUserProjections(id: string | undefined): ProjectionType<UserDoc> | null | undefined {
+  return {
+    friends: {
+      $elemMatch: {
+        id: { $eq: id },
+      },
+    },
+    requests: {
+      $elemMatch: {
+        id: { $eq: id },
+      },
+    },
+    invitations: {
+      $elemMatch: {
+        id: { $eq: id },
+      },
+    },
+    displayName: 1,
+    thumbnail: 1,
+    picture: 1,
+    "friends.id": 1,
+    "requests.id": 1,
+    "invitations.id": 1,
+  };
+}
